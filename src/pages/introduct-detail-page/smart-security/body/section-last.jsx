e0d1c21a2c1c8a017fd5b2d2697f31c119d00ebf
mivoli98 <mivoli98@tamu.edu>
2021-07-05 04:16:17 -0500

    Lighting+security+home+energy

--- src/pages/introduct-detail-page/smart-security/body/section-last.jsx
+++ src/pages/introduct-detail-page/smart-security/body/section-last.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import SectionOutstanding from '@components/section/section-outstanding ';
+import SectionHorizontalSlide from "@components/section/section-horizontal-slide/section-horizontal-slide";
+import { useTranslation } from 'react-i18next';
+import DataHomePage from "@query/home-page";
 const SectionIntroductLast = () => {
+    const { t } = useTranslation();
+    const data = DataHomePage();
     return (
         <div className="container-wrap section-outstanding">
-            <SectionOutstanding title={"TÊN CÔNG TRÌNH ĐƯỢC THI CÔNG"} />
+            <SectionHorizontalSlide data={data.dataSectionProject.edges} title={t(`Outstanding_Project`)} />
         </div>
     )
 }
