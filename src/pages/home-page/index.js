7a95403b229c4f52d06bb4de7fc021047ca23b5c
Kiệt <kietht@sunshinegroup.vn>
2021-07-20 10:56:16 +0000

    Update src/pages/home-page/index.js

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState,useEffect}from 'react';
 import SectionSlides from "./body/section-Slides";
 import Layout from "@components/layout.jsx";
 import Sectionorizontal from "@components/section/section-horizontal/section-horizontal";
