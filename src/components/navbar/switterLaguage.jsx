8897b1a6855bbd1fe35cf6008f689684a6b7319c
Kiệt <kietht@sunshinegroup.vn>
2021-11-24 02:19:51 +0000

    Revert "home page v2"
    This reverts commit 6c870add422cb4b12c950dd33ddf0ff62bade0b5

--- src/components/navbar/switterLaguage.jsx
+++ src/components/navbar/switterLaguage.jsx
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Dropdown } from "react-bootstrap";
 import { Language } from '@wapps/gatsby-plugin-i18next';
 import { useTranslation } from 'react-i18next';
-
-import iconLanguage from "../../images/new-home-page/header/icon-language.png"
-
 const isBrowser = typeof window !== "undefined";
 const LanguageSwitcher = ({ changeLng }) => {
     const valueFlag = ({ vn: "vn", en: "en" });
@@ -18,7 +15,7 @@ const LanguageSwitcher = ({ changeLng }) => {
     useEffect(() => {
         if (language === "vn") {
             setIsChange(true)
-        } else {
+        }else{
             setIsChange(false)
         }
         if (isBrowser) {
@@ -32,14 +29,13 @@ const LanguageSwitcher = ({ changeLng }) => {
                     i18n.changeLanguage("en");
                     setIsChange(false);
                 }
-
+                
             }
         }
     }, [language, isChange, i18n])
-    return <Dropdown className="dropdown-language dropdown-language-v2">
+    return <Dropdown className="dropdown-language">
         <Dropdown.Toggle className="drop-toggle dropdown-toggle" variant="#ffffff" id="dropdown-basic">
-            <span>{isChange ? 'VI' : 'EN'}</span>
-            <img src={iconLanguage} alt="" />
+            <span>  {isChange ? 'Việt Nam' : 'English'}</span>
         </Dropdown.Toggle>
         <Dropdown.Menu className="dropdown-customize" id="dropdown-customize">
             <Dropdown.Item onClick={() => { changeFlag(valueFlag.vn); changeLng(valueFlag.vn); }}>Việt Nam</Dropdown.Item>
