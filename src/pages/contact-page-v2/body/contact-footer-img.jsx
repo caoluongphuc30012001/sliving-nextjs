1326af69abc4f784ef1407c40ee96f9981c2f8d4
caoloc <caoloc2020cc@gmail.com>
2021-12-22 17:37:02 +0700

    fix: edit 'browser first err'

--- src/pages/contact-page-v2/body/contact-footer-img.jsx
+++ src/pages/contact-page-v2/body/contact-footer-img.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import bottomImage from "@images/contact-page-v2/image111.svg";
 import underLine from "@images/contact-page-v2/Rectangle1256.png";
+import { useTranslation } from "react-i18next";
 
 const BottomImage = () => {
+  const { t } = useTranslation();
   return (
     <section className="bottomImage mb-5">
       <div>
         <img src={bottomImage} alt="Bottom" />
         <div className="contentImage">
-          <h1>Revolution of intelligent illumination
+          <h1>{t(`Contact.Revolution`)}
             <img src={underLine} alt="underline" />
           </h1>
         </div>
