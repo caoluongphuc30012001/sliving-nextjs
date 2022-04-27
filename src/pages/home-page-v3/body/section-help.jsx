be542eda839f9ae4fa616ec9f04ee1ea38addaa0
OliverDoan <doanvietthuan1005@gmail.com>
2022-04-27 15:52:15 +0700

    add row help more

---
+++ src/pages/home-page-v3/body/section-help.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+const SectionHelpMore = () => {
+  return (
+    <section>
+      <div className="container-wrap">
+        <Row>
+          <Col className="grid-3">
+            <div className="a">a</div>
+            <div className="a">a</div>
+            <div className="a">a</div>
+          </Col>
+        </Row>
+      </div>
+    </section>
+  );
+};
+export default SectionHelpMore;
