6e492b8e59d11bd67d8589deb7fcc479fd3fb336
Hoa Doan <hoadvt@sunshinegroup.vn>
2022-05-13 18:31:54 +0700

    add page security

---
+++ src/pages/smart-security-v3/body/SectionSmartFaceID.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import imgLeft from "../../../images/smart-security-v3/img-face-id.png";
+import iconV from "../../../images/smart-security-v3/icon-tick-v-smart-security.svg";
+
+function SectionSmartFaceID() {
+  return (
+    <section className="bg-smart-door-locks">
+      <Container className="max-width-1180 px-0">
+        <Row className="padding-left-right-smart-door">
+          <h3 className="sub-title-smart-secuirity">FACE ID TIME ATTENDANCE</h3>
+          <h2 className="title-smart-secuirity">
+            Hệ Thống Chấm Công Bằng Khuôn Mặt
+          </h2>
+          <div>
+            <div className="div-under-smart-security"></div>
+          </div>
+          <Row className="gx-md-4 gy-4 px-0  ">
+            <Col md={6} className="px-0">
+              <div className="div-wrapper-img-smart-security">
+                <Image
+                  src={imgLeft}
+                  className="img-fluid img-left-smart-door-locks"
+                />
+              </div>
+            </Col>
+            <Col md={6} className="col-main-smart-door">
+              <p className="title-smart-door-1">Tiện Ích</p>
+              <p className="title-smart-door-2">
+                <Image src={iconV} />
+                <span>Nhận diện nhanh & chính xác</span>
+              </p>
+              <p className="sub-title-smart-door">
+                Có thể xác thực từ khoảng cách xa mà không cần tiếp xúc thiết bị
+                với tốc độ nhận diện gương mặt nhanh chóng và chính xác.
+              </p>
+              <p className="title-smart-door-2">
+                <Image src={iconV} />
+                <span>Tính bảo mật cao</span>
+              </p>
+              <p className="sub-title-smart-door">
+                Giải pháp chấm công hiện đại với độ bảo mật cao, tránh được tình
+                trạng rò rỉ thông tin và hỗ trợ quản lý, xử lý dữ liệu an toàn.
+              </p>
+              <p className="title-smart-door-2">
+                <Image src={iconV} />
+                <span>Dễ dàng tích hợp</span>
+              </p>
+              <p className="sub-title-smart-door">
+                Có thể tích hợp FaceID với hệ thống thiết bị thông minh ở cửa ra
+                vào, thang máy, khu chung cư, toà nhà nhằm mục đích tăng cường
+                bảo mật và chấm công tiện lợi.
+              </p>
+            </Col>
+          </Row>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+export default SectionSmartFaceID;
---
+++ src/pages/smart-security-v3/body/SectionTopSmartSecurity.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import { Link } from 'react-router-dom';
+import { Container, Row, Col, Image } from "react-bootstrap";
+import imgSecurity from "../../../images/smart-security-v3/img-top-security.png";
+
+function SectionTopSmartSecurity() {
+  return (
+    <section className="bg-top-smart-security">
+      <Container className="max-width-1600 px-0">
+        <Row className="gx-md-5 padding-left-right row-main-top-security">
+          <Col md={6} className="col-img-top-security">
+            <Image src={imgSecurity} className="img-fluid img-top-security" />
+          </Col>
+          <Col md={6} className="order-first order-md-last">
+            <h2 className="title-top-smart-security">SMART SECURITY</h2>
+            <p className="detail-top-smart-security">
+              Chúng tôi tự hào khi mang đến cho khách hàng một hàng rào an ninh
+              ứng dụng công nghệ số kiên cố, hiện đại, vững chắc và an toàn cho
+              các khu dân cư, dự án và tòa nhà.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+export default SectionTopSmartSecurity;
