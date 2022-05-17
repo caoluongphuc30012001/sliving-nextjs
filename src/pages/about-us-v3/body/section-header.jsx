e59c83d915f7ea47fe0f9a08784711063028258d
RiverO2442 <66553338+RiverO2442@users.noreply.github.com>
2022-05-17 17:53:22 +0700

    update swiper and header

--- src/pages/about-us-v3/body/section-header.jsx
+++ src/pages/about-us-v3/body/section-header.jsx
@@ -3,18 +3,21 @@ import background from "../../../images/about-us-v3/png/about-us-banner.png";
 
 const SectionHeader = () => {
   return (
-    <header className="about-us-v3">
-      <div className="about-us-content">
-        <h1 className="about-us-title">VỀ CHÚNG TÔI</h1>
-        <p className="about-us-subtitle">
-          Hệ sinh thái sản phẩm mang thương hiệu S-living thuộc tập đoàn công
-          nghệ Unicloud hướng tới cung cấp cho khách hàng một trải nghiệm sống
-          tiện nghi, hiện đại, liền mạch thông qua việc ứng dụng các công nghệ
-          tiên tiến vào các giải pháp sản phẩm smart home, smart lighting, smart
-          parking và smart security
-        </p>
+    <header className="section-about-us-header">
+      <div className="header-container">
+        <div className="header-content">
+          <h1 className="content-title">VỀ CHÚNG TÔI</h1>
+          <div className="content-subtitle-container">
+            <p className="content-subtitle">
+              Hệ sinh thái sản phẩm mang thương hiệu S-living thuộc tập đoàn
+              công nghệ Unicloud hướng tới cung cấp cho khách hàng một trải
+              nghiệm sống tiện nghi, hiện đại, liền mạch thông qua việc ứng dụng
+              các công nghệ tiên tiến vào các giải pháp sản phẩm smart home,
+              smart lighting, smart parking và smart security
+            </p>
+          </div>
+        </div>
       </div>
-      <img className="about-us-background" src={background} />
     </header>
   );
 };
