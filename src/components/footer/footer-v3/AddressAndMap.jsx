d21ea8037bf80fcd8dab4658fa0add72a548b785
OliverDoan <doanvietthuan1005@gmail.com>
2022-05-19 08:32:03 +0700

    fix error window

--- src/components/footer/footer-v3/AddressAndMap.jsx
+++ src/components/footer/footer-v3/AddressAndMap.jsx
@@ -8,28 +8,45 @@ import SectionMap from "./map";
 import iconBgPin from "@images/footer-v3/svg/icon-bg-pin.svg";
 import iconBgEmail from "@images/footer-v3/svg/icon-bg-mail.svg";
 import iconBgPhone from "@images/footer-v3/svg/icon-bg-phone.svg";
+import { useMemo } from "react";
 
-export default function AddressAndMap() {
-  // const { t } = useTranslation();
+const isBrowser = typeof window !== "undefined";
 
-  const listContact = [
-    {
-      key: "Trụ sở chính Hà Nội",
-      urlIcon: iconBgPin,
-      position: { lat: 21.0300541, lng: 105.7786784 },
+const listContact = [
+  {
+    key: {
+      title: "Trụ sở chính",
       description: "Tầng 10, Toà nhà Sunshine Center, Số 16 Phạm Hùng, Hà Nội",
     },
-    {
-      key: "Chi nhánh TP.Hồ Chí Minh",
-      urlIcon: iconBgPin,
-      position: { lat: 10.849997413384632, lng: 106.80553665690692 },
+    urlIcon: iconBgPin,
+    position: { lat: 21.0300541, lng: 105.7786784 },
+  },
+  {
+    key: {
+      title: "Chi nhánh TP.Hồ Chí Minh",
       description:
         "Block 5, Lô I-3B-1, Đường N6, Khu Công Nghệ Cao, Phường Tân Phú, Thành phố Thủ Đức, Thành phố Hồ Chí Minh",
     },
-    { key: "Email", urlIcon: iconBgEmail, description: "info@unicloud.com.vn" },
-    { key: "Hotline", urlIcon: iconBgPhone, description: "19006054" },
-  ];
-
+    urlIcon: iconBgPin,
+    position: { lat: 10.849997413384632, lng: 106.80553665690692 },
+  },
+  {
+    key: {
+      title: "Email",
+      description: "info@unicloud.com.vn",
+    },
+    urlIcon: iconBgEmail,
+  },
+  {
+    key: {
+      title: "Hotline",
+      description: "19006054",
+    },
+    urlIcon: iconBgPhone,
+  },
+];
+export default function AddressAndMap() {
+  // const { t } = useTranslation();
   const [position, setPosition] = useState();
   const [map, setMap] = useState();
 
@@ -52,21 +69,26 @@ export default function AddressAndMap() {
     },
     [map]
   );
-
+  const checkBrowser = useMemo(() => {
+    if (isBrowser)
+      return <SectionMap position={position} whenCreated={onMapCreated} />;
+    else return [];
+  }, [isBrowser]);
   return (
     <Container className="map-detail container-child">
-      <Row className="m-left-right-12">
+      <Row className="g-lg-5 g-sm-3">
         <Col sm={12} lg={6}>
-          <SectionMap position={position} whenCreated={onMapCreated} />
+          {/* <SectionMap position={position} whenCreated={onMapCreated} /> */}
+          {checkBrowser}
         </Col>
         <Col sm={12} lg={6}>
-          <h3 className="fs-4 fw-bold text-gradient-origan map-detail-address-footer ">
-            ĐỊA CHỈ LIÊN HỆ
+          <h3 className="fs-4 fw-bold text-gradient-origan map-detail-address-footer">
+            Kết nối với chúng tôi
           </h3>
           <div className="group-contact">
             {listContact.map((contact) => (
               <div
-                key={contact.key}
+                key={contact.key.title}
                 className="group-contact-wrap d-flex align-items-center mb-3"
                 role="button"
                 onClick={() => onSelectContact(contact)}
@@ -76,11 +98,15 @@ export default function AddressAndMap() {
                   src={contact.urlIcon}
                   alt=""
                   width={48}
-                  className={`${contact.key ? "align-self-start" : ""}`}
+                  className={"align-self-start"}
                 />
                 <div className="contact-wrap-item">
-                  <h5 className="fw-bold fs-18 title-contact">{contact.key}</h5>
-                  <p className="title-contact fs-6">{contact.description}</p>
+                  <h5 className="fw-bold fs-18 title-contact">
+                    {contact.key.title}
+                  </h5>
+                  <p className="title-contact fs-6">
+                    {contact.key.description}
+                  </p>
                 </div>
               </div>
             ))}
