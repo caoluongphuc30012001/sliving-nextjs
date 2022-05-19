bf3f2f2df2b722704f3450f2179a922b3500f116
Hoa Doan <hoadvt@sunshinegroup.vn>
2022-05-19 14:51:07 +0700

    add modal advise and modal thanks  to menu

--- src/components/menu/menu-top.jsx
+++ src/components/menu/menu-top.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "gatsby";
 import search from "../../images/smart-home-v3/svg/search.svg";
 import dropDown from "../../images/smart-home-v3/svg/arrow-down.svg";
-import { Link } from "gatsby";
-import './style.scss'
+import ModalAdvise from "../modal/modal-advise/ModalAdvise";
+
+import "./style.scss";
 const MenuTop = ({ logo, listRoute }) => {
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
     <nav className="section-menu-top">
       <div className="menu-top-container">
@@ -15,21 +19,16 @@ const MenuTop = ({ logo, listRoute }) => {
         <div className="list-item-menu">
           {listRoute.map((item) => {
             return item.listRoute ? (
-              <div
-                className="item-menu select-menu-top"
-                key={item.id}
-              >
+              <div className="item-menu select-menu-top" key={item.id}>
                 <div className="main-item-select item-select">
                   <p>{item.nameRoute}</p>
-                  <img src={dropDown} alt=""/>
+                  <img src={dropDown} alt="" />
                 </div>
                 <div className="drop-box">
                   {item.listRoute.map((item) => {
                     return (
                       <Link to={item.href} key={item.id}>
-                        <div className="item-select">
-                          {item.nameRoute}
-                        </div>
+                        <div className="item-select">{item.nameRoute}</div>
                       </Link>
                     );
                   })}
@@ -45,12 +44,13 @@ const MenuTop = ({ logo, listRoute }) => {
           })}
         </div>
         <div className="support-box">
-          <button className="btn-advise">
+          <button className="btn-advise" onClick={() => setModalShow(true)}>
             <div className="content">TƯ VẤN NGAY</div>
           </button>
           <img src={search} alt="" className="search" />
         </div>
       </div>
+      <ModalAdvise show={modalShow} onHide={() => setModalShow(false)} />
     </nav>
   );
 };
