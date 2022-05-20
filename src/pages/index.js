899b50ec616ffa501b88e777c6e4225570532d6e
Hoa Doan <hoadvt@sunshinegroup.vn>
2022-05-20 17:30:40 +0700

    fix import boostrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HomePageV3 from "./home-page-v3/index";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 const IndexPage = (props) => {
   return <HomePageV3 />;
