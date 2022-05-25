dd224a388816566361350bc61c28d26769de2606
Hoa Doan <hoadvt@sunshinegroup.vn>
2022-05-25 15:01:07 +0700

    update slide video components

--- src/pages/smart-home-v3/body/section-swiper-video-sliving.jsx
+++ src/pages/smart-home-v3/body/section-swiper-video-sliving.jsx
@@ -1,59 +1,33 @@
-import React from 'react'
-import SectionSwiperVideo from '../../../components/video-swiper/section-swiper-video';
-import video from '../../../images/smart-home-v3/jpg/video.jpg'
-import '../style.scss';
+import React from "react";
+import SectionSwiperVideo from "../../../components/video-swiper/section-swiper-video";
+import thumb1 from "../../../images/smart-home-v3/png/thumb-sliving-01.png";
+import thumb2 from "../../../images/smart-home-v3/png/thumb-sliving-02.png";
+import thumb3 from "../../../images/smart-home-v3/png/thumb-sliving-03.png";
+import thumb4 from "../../../images/smart-home-v3/png/thumb-sliving-04.png";
+
+import "../style.scss";
 export default function SectionSwiperVideoSliving() {
-    const listVideo = [
-        {
-            id: 0,
-            image:video
-        },
-        {
-            id: 1,
-            image:video
-        },
-        {
-            id: 2,
-            image:video
-        },
-        {
-            id: 3,
-            image:video
-        },
-        {
-            id: 4,
-            image:video
-        },
-        {
-            id: 5,
-            image:video
-        },
-        {
-            id: 6,
-            image:video
-        },
-        {
-            id: 7,
-            image:video
-        },
-        {
-            id: 8,
-            image:video
-        },
-        {
-            id: 9,
-            image:video
-        },
-        {
-            id:10,
-            image:video
-        },
-        {
-            id: 11,
-            image:video
-        },
-    ]
-    return (
-        <SectionSwiperVideo listVideo={listVideo} />
-    )
+  const listVideo = [
+    {
+      id: 0,
+      image: thumb1,
+      url: "https://data.unicloud.com.vn/wp-content/uploads/2022/05/video-sliving-1.mp4",
+    },
+    {
+      id: 1,
+      image: thumb2,
+      url: "https://data.unicloud.com.vn/wp-content/uploads/2022/05/video-sliving-2.mp4",
+    },
+    {
+      id: 2,
+      image: thumb3,
+      url: "https://data.unicloud.com.vn/wp-content/uploads/2022/05/video-sliving-3.mp4",
+    },
+    {
+      id: 3,
+      image: thumb4,
+      url: "https://data.unicloud.com.vn/wp-content/uploads/2022/05/video-sliving-4.mp4",
+    },
+  ];
+  return <SectionSwiperVideo listVideo={listVideo} />;
 }
