acd8d6230d3e4e1ddbc77579f3e5f5f36df9a690
viettq <viettq26701@gmail.com>
2022-07-06 15:05:23 +0700

    fix

--- src/components/layout-v3.jsx
+++ src/components/layout-v3.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 import ButtonRounded from "@components/button/button-card-rounded";
 import Navbar from "@components/menu/index";
@@ -11,91 +11,93 @@ function LayoutV3({ children }) {
   // const url = typeof window !== "undefined" ? window.location.href : "";
   const location = useLocation();
   let url = location.href?location.href:"";
-  console.log("href ", typeof url);
-  const listRoute = [
-    {
-      id: 0,
-      nameRoute: "TRANG CHỦ",
-      href: "/",
-      active:
-        !url.includes("about-us") &&
-        !url.includes("implementation-process") &&
-        !url.includes("step") &&
-        !url.includes("business") &&
-        !url.includes("news") &&
-        !url.includes("contact-us"),
-    },
-    {
-      id: 1,
-      nameRoute: "VỀ CHÚNG TÔI",
-      href: "/about-us/",
-      active: url.includes("about-us"),
-    },
-    {
-      id: 2,
-      nonActive: true,
-      nameRoute: "GIẢI PHÁP",
-      listRoute: [
-        {
-          id: 0,
-          nameRoute: "SMART HOME",
-          href: "/smart-home/",
-        },
-        {
-          id: 1,
-          nameRoute: "SMART LIGHTING",
-          href: "/smart-lighting/",
-        },
-        {
-          id: 2,
-          nameRoute: "SMART PARKING",
-          href: "/smart-parking/",
-        },
-        {
-          id: 3,
-          nameRoute: "SMART BUILDING",
-          href: "/smart-building/",
-        },
-      ],
-    },
-    {
-      id: 3,
-      nameRoute: "GÓI GIẢI PHÁP",
-      nonActive: true,
-      listRoute: [
-        {
-          id: 0,
-          nameRoute: "GÓI DOANH NGHIỆP",
-          href: "/business/",
-          active: url.includes("business"),
-        },
-        {
-          id: 1,
-          nameRoute: "GÓI CÁ NHÂN",
-          href: "/personal-step1/",
-          active: url.includes("personal"),
-        },
-      ],
-    },
-    {
-      id: 4,
-      nameRoute: "TIN TỨC",
-      href: "https://unicloud.com.vn/news",
-      active: url.includes("news"),
-    },
-    {
-      id: 5,
-      nameRoute: "QUY TRÌNH",
-      href: "/implementation-process/",
-      active: url.includes("implementation-process"),
-    },
-    {
-      id: 6,
-      nameRoute: "LIÊN HỆ",
-      href: "/contact-us",
-      active: url.includes("contact-us"),
-    },
-  ];
+  const [listRoute,setListRoute] = useState([])
+  useEffect(()=>{
+    setListRoute([
+      {
+        id: 0,
+        nameRoute: "TRANG CHỦ",
+        href: "/",
+        active:
+          !url.includes("about-us") &&
+          !url.includes("implementation-process") &&
+          !url.includes("step") &&
+          !url.includes("business") &&
+          !url.includes("news") &&
+          !url.includes("contact-us"),
+      },
+      {
+        id: 1,
+        nameRoute: "VỀ CHÚNG TÔI",
+        href: "/about-us/",
+        active: url.includes("about-us"),
+      },
+      {
+        id: 2,
+        nonActive: true,
+        nameRoute: "GIẢI PHÁP",
+        listRoute: [
+          {
+            id: 0,
+            nameRoute: "SMART HOME",
+            href: "/smart-home/",
+          },
+          {
+            id: 1,
+            nameRoute: "SMART LIGHTING",
+            href: "/smart-lighting/",
+          },
+          {
+            id: 2,
+            nameRoute: "SMART PARKING",
+            href: "/smart-parking/",
+          },
+          {
+            id: 3,
+            nameRoute: "SMART BUILDING",
+            href: "/smart-building/",
+          },
+        ],
+      },
+      {
+        id: 3,
+        nameRoute: "GÓI GIẢI PHÁP",
+        nonActive: true,
+        listRoute: [
+          {
+            id: 0,
+            nameRoute: "GÓI DOANH NGHIỆP",
+            href: "/business/",
+            active: url.includes("business"),
+          },
+          {
+            id: 1,
+            nameRoute: "GÓI CÁ NHÂN",
+            href: "/personal-step1/",
+            active: url.includes("personal"),
+          },
+        ],
+      },
+      {
+        id: 4,
+        nameRoute: "TIN TỨC",
+        href: "https://unicloud.com.vn/news",
+        active: url.includes("news"),
+      },
+      {
+        id: 5,
+        nameRoute: "QUY TRÌNH",
+        href: "/implementation-process/",
+        active: url.includes("implementation-process"),
+      },
+      {
+        id: 6,
+        nameRoute: "LIÊN HỆ",
+        href: "/contact-us",
+        active: url.includes("contact-us"),
+      },
+    ])
+  },[url])
   return (
     <>
       <Navbar listRoute={listRoute} logo={logo} />
