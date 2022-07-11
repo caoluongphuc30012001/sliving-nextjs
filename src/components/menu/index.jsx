d6e638678c64dce5037eed92bdc231e333ed2af4
Phúc Cao Lương <phuccao.30012001@gmail.com>
2022-07-11 10:38:28 +0700

    fix/navbar

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import MenuTop from "./menu-top";
 import MenuTopMobile from "./menu-top-mobile";
 
-export default function Menu({ logo, listRoute, back }) {
+export default function Menu({ logo, listRoute, back, business }) {
   return (
     <>
-      <MenuTop listRoute={listRoute} logo={logo} back={back} />
-      <MenuTopMobile listRoute={listRoute} logo={logo} back={back} />
+      <MenuTop
+        listRoute={listRoute}
+        logo={logo}
+        back={back}
+        business={business}
+      />
+      <MenuTopMobile
+        listRoute={listRoute}
+        logo={logo}
+        back={back}
+        business={business}
+      />
     </>
   );
 }
