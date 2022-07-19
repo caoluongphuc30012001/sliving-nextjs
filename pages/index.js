b09cfe294cd258900718b7d78e9b117f62c64e7c
phuccao.30012001@gmail.com <phuccao.30012001@gmail.com>
2022-07-19 14:18:47 +0700

    set up folder

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Header from "@components/header";
+import React from "react";
+import HomePage from "./home-page";
 export default function Home() {
   return (
     <div className="app">
-      <Header />
+      <HomePage />
     </div>
   );
 }
