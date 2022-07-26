c7adec37b956c57d0b6ed7dd372f7fb895e87943
chauminhquang <quangcm@unicloud.com.vn>
2022-07-26 13:21:49 +0700

    add translating path config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,14 @@
+//const path = require("path");
+// const LOCALE_STRUCTURE = "{{lng}}/{{ns}}";
+// const LOCALE_PATH = "./public/abc/locales";
+const DEFAULT_LANGUAGE = "vi";
+
 module.exports = {
   i18n: {
-    defaultLocale: "vi",
+    defaultLocale: DEFAULT_LANGUAGE,
     localeDetection: false,
     locales: ["vi", "en"],
   },
+  // localePath: path.resolve(LOCALE_PATH),
+  // localeStructure: LOCALE_STRUCTURE,
 };
--- pages/home-page/index.js
+++ pages/home-page/index.js
@@ -15,20 +15,9 @@ export const getStaticProps = async ({ locale }) => {
 const HomePage = (props) => {
   const { t } = useTranslation("home");
 
-  const [lng, setLng] = useState("en");
-
-  useEffect(() => {
-    i18n.changeLanguage(lng);
-  }, [lng]);
-
-  const handleClick = () => {
-    setLng("en");
-  };
-
   return (
     <div>
       <h1 style={{ color: "red" }}>{t("hello")}</h1>
-      <button onClick={handleClick}>Click me</button>
     </div>
   );
 };
