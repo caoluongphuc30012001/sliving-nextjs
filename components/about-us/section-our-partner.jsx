681a805e81c721b8a19b1e3bd7ae2aa93075285e
RiverO <nguyen.anhhao.840@gmail.com>
2022-07-28 17:34:19 +0700

    add about us and business

---
+++ components/about-us/section-our-partner.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import imgUnderLine from '@images/home-page-v3/svg/title-line-under.svg';
+import iconPartner1 from '@images/about-us-v3/svg/icon-partner1.svg';
+import iconPartner2 from '@images/about-us-v3/svg/icon-partner2.svg';
+import iconPartner3 from '@images/about-us-v3/svg/icon-partner3.svg';
+import iconPartner4 from '@images/about-us-v3/svg/icon-partner4.png';
+import iconPartner5 from '@images/about-us-v3/svg/icon-partner5.png';
+import iconPartner6 from '@images/about-us-v3/svg/icon-partner6.svg';
+import iconPartner7 from '@images/about-us-v3/svg/icon-partner7.svg';
+import iconPartner8 from '@images/about-us-v3/svg/icon-partner8.svg';
+import iconPartner9 from '@images/about-us-v3/svg/icon-partner9.svg';
+import iconPartner10 from '@images/about-us-v3/svg/icon-partner10.svg';
+import iconPartner11 from '@images/about-us-v3/svg/icon-partner11.svg';
+import iconPartner12 from '@images/about-us-v3/svg/icon-partner12.png';
+
+import { useTranslation } from 'react-i18next';
+
+import Image from 'next/image';
+import style from './style.module.scss';
+function SectionOurPartner() {
+  const { t } = useTranslation('aboutUs');
+
+  const listItemLogo = [
+    {
+      id: 1,
+      linkIconLogo: iconPartner1,
+    },
+    {
+      id: 2,
+      linkIconLogo: iconPartner2,
+    },
+    {
+      id: 3,
+      linkIconLogo: iconPartner3,
+    },
+    {
+      id: 4,
+      linkIconLogo: iconPartner4,
+    },
+    {
+      id: 5,
+      linkIconLogo: iconPartner5,
+    },
+    {
+      id: 6,
+      linkIconLogo: iconPartner6,
+    },
+    {
+      id: 7,
+      linkIconLogo: iconPartner7,
+    },
+    {
+      id: 8,
+      linkIconLogo: iconPartner8,
+    },
+    {
+      id: 9,
+      linkIconLogo: iconPartner9,
+    },
+    {
+      id: 10,
+      linkIconLogo: iconPartner10,
+    },
+    {
+      id: 11,
+      linkIconLogo: iconPartner11,
+    },
+    {
+      id: 12,
+      linkIconLogo: iconPartner12,
+    },
+  ];
+  return (
+    <section className={style[`bg-section-our-partner`]}>
+      <div className={`${style[`max-width-1180`]}  ${style[`our-partner-container`]}`}>
+        <Row className={style[`top-content`]}>
+          <span className={style[`title`]}>OUR PARTNERS</span>
+          <h2 className={style[`main-title`]}>{t('aboutUs.ourPartner.mainTitle')}</h2>
+          <div className={style[`underline`]}>
+            <Image src={imgUnderLine} style={{ width: 88, height: 4 }} />
+          </div>
+          <p className={style[`description-line-center`]}>{t('aboutUs.ourPartner.mainDesc')}</p>
+        </Row>
+        <Row lg={6} xs={4} className={style[`logo-items`]}>
+          {listItemLogo.map((itemLogo) => (
+            <Col key={itemLogo.id} className={style[`logo-item-content`]}>
+              <div className={style[`logo-item-img`]}>
+                <Image src={itemLogo.linkIconLogo} className={style[``]} />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </section>
+  );
+}
+
+export default SectionOurPartner;
---
+++ components/business/section-product-list.jsx
@@ -0,0 +1,201 @@
+import React, { useState, useContext, useEffect } from 'react';
+
+// import { BusinessStateContext } from "@context/businessContext";
+// import ModalAdvise from "@components/modal/modal-advise/ModalAdvise-business";
+import icon from '@images/business-step3-v3/png/icon-vector.png';
+
+import { useTranslation } from 'react-i18next';
+
+import style from './style.module.scss';
+import Image from 'next/image';
+
+const Table2 = ({ table }) => {
+  const { t } = useTranslation('business');
+  return (
+    <div className={style[`table`]}>
+      <table className={style[`table-container`]}>
+        <thead className={style[`content-container`]}>
+          <tr className={style[`table-row`]}>
+            <th className={`${style[`table-data`]} ${style[`header`]}`}>
+              {t('business.packagesData.solution')}
+            </th>
+            <th className={`${style[`table-data`]} ${style[`header`]} ${style[`center`]}`}>
+              {t('business.packagesData.device')}
+            </th>
+            <th className={`${style[`table-data`]} ${style[`header`]}`}>
+              {t('business.packagesData.normal')}
+            </th>
+            <th className={`${style[`table-data`]} ${style[`header`]}`}>
+              {t('business.packagesData.advanced')}
+            </th>
+          </tr>
+        </thead>
+        <tbody className={`${style[`body-content-container`]} ${style[`border-b`]}`}>
+          <tr className={style[`body-table-row`]}>{table.solutionName}</tr>
+          <tr className={`${style[`body-table-row`]} ${style[`border-lr`]}`}>
+            {table.deviceList.map((item) => {
+              return (
+                <td key={item.id} className={`${style[`table-data`]} ${style[`border-b`]}`}>
+                  {item.deviceName}
+                </td>
+              );
+            })}
+          </tr>
+          <tr className={style[`body-table-row`]}>
+            {table.deviceList.map((item, index) => {
+              return (
+                <td
+                  key={index}
+                  id={item.id}
+                  className={`${style[`table-data`]} ${style[`border-b`]}`}
+                >
+                  {item.inBasic && (
+                    <Image alt="" className={style[`table-data-icon`]} src={icon}></Image>
+                  )}
+                </td>
+              );
+            })}
+          </tr>
+          <tr className={`${style[`body-table-row`]} ${style[`border-lr`]}`}>
+            {table.deviceList.map((item, index) => {
+              return (
+                <td
+                  key={index}
+                  id={item.id}
+                  className={`${style[`table-data`]} ${style[`border-b`]}`}
+                >
+                  {item.inAdvanced && (
+                    <Image alt="" className={style[`table-data-icon`]} src={icon}></Image>
+                  )}
+                </td>
+              );
+            })}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const SubTable = ({ table }) => {
+  const { t } = useTranslation('business');
+  return (
+    <tbody className={`${style[`body-content-container`]} ${style[`border-b`]}`}>
+      <tr className={`${style[`body-table-row`]}`}>{table.solutionName}</tr>
+      <tr className={`${style[`body-table-row`]} ${style[`border-lr`]}`}>
+        <td className={`${style[`table-data`]} ${style[`border-b`]}`}>{table.softwareName}</td>
+        {table.deviceList.content.length > 0 && (
+          <td className={`${style[`table-data-center`]}`}>
+            <div className={`${style[`data-col`]} ${style[`border-r`]}`}>
+              {table.deviceList.listName}
+            </div>
+            <td className={`${style[`data-col`]}`}>
+              {table.deviceList.content?.map((item, index) => {
+                const id = index + 1;
+                return (
+                  <td key={id} className={`${style[`table-data`]} ${style[`border-b`]}`}>
+                    {item.deviceName}
+                  </td>
+                );
+              })}
+            </td>
+          </td>
+        )}
+      </tr>
+      <tr className={`${style[`body-table-row`]}`}>
+        <td className={`${style[`table-data`]}`}>
+          <div className={`${style[`quantity-data`]}`}>{t('business.packagesData.contact')}</div>
+        </td>
+      </tr>
+    </tbody>
+  );
+};
+
+const SectionProductList = () => {
+  const [modalShow, setModalShow] = useState(false);
+  // const [activeTable, setActiveTable] = useState(false);
+  // const [order, setOrder] = useState("ASC");
+  const [tableData, setTableData] = useState('');
+  // const state = useContext(BusinessStateContext);
+  // useLayoutEffect(() => {
+  //   if (!state["total"]) navigate("/business-step1");
+  // }, []);
+  const [solutionNames, setSolutionNames] = useState('');
+
+  const { i18n, t, ready } = useTranslation('business');
+  useEffect(() => {
+    const data = t('business.tableData2', { returnObjects: true });
+    setTableData(data);
+  }, [ready, i18n.language]);
+
+  const sorting = (col) => {
+    // if (order === "ASC") {
+    //   const sorted = tableData;
+    //   sorted.content[`${activeTable}`].content.sort((a, b) =>
+    //     b[col].localeCompare(a[col])
+    //   );
+    //   setTableData(sorted);
+    //   setOrder("DESC");
+    // } else {
+    //   const sorted = tableData;
+    //   sorted.content[`${activeTable}`].content.sort((a, b) =>
+    //     a[col].localeCompare(b[col])
+    //   );
+    //   setTableData(sorted);
+    //   setOrder("ASC");
+    // }
+  };
+  return (
+    <section className={style[`section-business-product-list`]}>
+      <div className={style[`section-container`]}>
+        <div className={style[`content-title`]}>
+          <div className={style[`sub-title`]}>{t('business.packagesData.listDeviceSubTitle')}</div>
+          <div className={style[`title`]}>{t('business.packagesData.listDeviceTitle')}</div>
+          <div className={style[`underline`]}></div>
+        </div>
+        {tableData.content?.map((table) => {
+          return <Table2 key={table.id} table={table} />;
+        })}
+        <div className={style[`sub-table`]}>
+          <table className={style[`table-container`]}>
+            <thead className={style[`content-container`]}>
+              <tr className={style[`table-row`]}>
+                <th className={`${style[`table-data`]} ${style[`header`]}`}>
+                  {t('business.packagesData.solution')}
+                </th>
+                <th className={`${style[`table-data`]} ${style[`header`]} ${style[`center`]}`}>
+                  {t('business.packagesData.device')}
+                </th>
+                <th className={`${style[`table-data`]} ${style[`header`]} ${style[`center`]}`}>
+                  {t('business.packagesData.count')}
+                </th>
+              </tr>
+            </thead>
+            {tableData?.subContent?.solutionList?.map((table) => {
+              return <SubTable sorting={sorting} key={table.id} table={table} />;
+            })}
+          </table>
+        </div>
+        <div className={style[`sumary`]}>
+          <div className={style[`smallNote`]}>{tableData.smallNote}</div>
+        </div>
+        <div
+          className={style[`advise-now-btn`]}
+          onClick={() => setModalShow(true)}
+          onKeyDown={() => {}}
+          role="button"
+          tabIndex={0}
+        >
+          <span>{t('sectionDropdown.consultNow')}</span>
+        </div>
+      </div>
+      {/* <ModalAdvise
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        solutionNames={solutionNames}
+      /> */}
+    </section>
+  );
+};
+
+export default SectionProductList;
